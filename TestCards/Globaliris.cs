08704f3519c4f2f8ea04176ffd7a0acfd61cd8d7
maxmigov <maxim.migov@aspira.software>
2021-01-19 17:48:03 +0200

    card details changes

--- TestCards/Globaliris.cs
+++ TestCards/Globaliris.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace CommifyMSTestFramework.TestCards.SecureTrading
+namespace CommifyMSTestFramework.TestCards.Globaliris
 {
 	public class VisaDebit //checked
 	{
@@ -20,7 +18,7 @@ namespace CommifyMSTestFramework.TestCards.SecureTrading
 	public class VisaCredit //checked
 	{
 		public static string credit_card_holder = "TestVisaCredit";
-		public static string credit_card_number = "4484000000000411";
+		public static string credit_card_number = "4263970000005262";
 		public static string credit_card_valid_month = "12";
 		public static string credit_card_valid_year = "19";
 		public static string credit_card_expiry_month = "12";
@@ -31,7 +29,7 @@ namespace CommifyMSTestFramework.TestCards.SecureTrading
 	public class MasterCardDebit //checked
 	{
 		public static string debit_card_holder = "TestMasterCardDebit";
-		public static string debit_card_number = "5124990000000101";
+		public static string debit_card_number = "5555555555554444";
 		public static string debit_card_valid_month = "12";
 		public static string debit_card_valid_year = "19";
 		public static string debit_card_expiry_month = "12";
@@ -42,7 +40,7 @@ namespace CommifyMSTestFramework.TestCards.SecureTrading
 	public class MasterCardCredit //checked
 	{
 		public static string credit_card_holder = "TestMasterCardCredit";
-		public static string credit_card_number = "5100000000000511";
+		public static string credit_card_number = "5425230000004415";
 		public static string credit_card_valid_month = "12";
 		public static string credit_card_valid_year = "19";
 		public static string credit_card_expiry_month = "12";
@@ -53,7 +51,7 @@ namespace CommifyMSTestFramework.TestCards.SecureTrading
 	public class Amex //checked
 	{
 		public static string amex_card_holder = "TestAmex";
-		public static string amex_card_number = "340000000000611";
+		public static string amex_card_number = "374101000000608";
 		public static string amex_card_valid_month = "12";
 		public static string amex_card_valid_year = "19";
 		public static string amex_card_expiry_month = "12";
@@ -64,7 +62,7 @@ namespace CommifyMSTestFramework.TestCards.SecureTrading
 	public class Failed //checked
 	{
 		public static string failed_card_holder = "TestFailed";
-		public static string failed_card_number = "4111110000000112";
+		public static string failed_card_number = "4000120000001154";
 		public static string failed_card_valid_month = "12";
 		public static string failed_card_valid_year = "19";
 		public static string failed_card_expiry_month = "12";
