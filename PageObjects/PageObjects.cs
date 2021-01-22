4df1da677c44c02fd9bb756ed5bff06d8997158c
maxmigov <maxim.migov@aspira.software>
2021-01-22 09:45:41 +0200

    delete page object

--- PageObjects/PageObjects.cs
+++ PageObjects/PageObjects.cs
@@ -40,8 +40,6 @@ namespace CommifyMSTestFramework.PageObjects
         public IWebElement Logo => driver.FindElement(By.XPath("/html/body/div/div[1]/div/div/div/img"));
         public IWebElement BodyHeadCardDetails => driver.FindElement(By.XPath("/html/body/div/div[2]/div[2]/h4")); //first row
         public IWebElement BodyHeadDescription => driver.FindElement(By.XPath("/html/body/div/div[2]/div[2]/p")); //second row
-        public IWebElement BodyHeadDescription2 => driver.FindElement(By.Id("description2")); //third row (debit only tipa)
-
         //shield to add
         public IWebElement PaymentAmountDescription => driver.FindElement(By.XPath("/html/body/div/div[2]/div[3]/form/div[2]/div[1]/div[3]/div[1]"));
         public IWebElement SecureSSLDescription => driver.FindElement(By.XPath("/html/body/div/div[2]/div[3]/form/div[2]/div[1]/div[1]/div[2]"));
