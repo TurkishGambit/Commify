a8a7f35039c52dd5248c567b0cb58f74229633f7
maxmigov <maxim.migov@aspira.software>
2021-01-23 17:13:16 +0200

    paysafe3ds enrolled cards added

--- TestCards/PaySafe3DS.cs
+++ TestCards/PaySafe3DS.cs
@@ -4,7 +4,60 @@ using System.Text;
 
 namespace CommifyMSTestFramework.TestCards.PaySafe3DS
 {
-	class Test
+	public class EnrolledVisaDebit //checked
 	{
+		public static string debit_card_holder = "TestEnrolledVisaDebit";
+		public static string debit_card_number = "4835641100110000";
+		public static string debit_card_valid_month = "12";
+		public static string debit_card_valid_year = "19";
+		public static string debit_card_expiry_month = "12";
+		public static string debit_card_expiry_year = "21";
+		public static string debit_card_cvc = "111";
 	}
+
+	public class EnrolledVisaCredit //checked
+	{
+		public static string credit_card_holder = "TestEnrolledVisaCredit";
+		public static string credit_card_number = "4500030000000004";
+		public static string credit_card_valid_from_month = "12";
+		public static string credit_card_valid_from_year = "19";
+		public static string credit_card_expiry_month = "12";
+		public static string credit_card_expiry_year = "21";
+		public static string credit_card_cvc = "111";
+	}
+
+	public class EnrolledMasterCardDebit //checked
+	{
+		public static string debit_card_holder = "TestEnrolledMasterCardDebit";
+		public static string debit_card_number = "5573560100022200";
+		public static string debit_card_valid_from_month = "12";
+		public static string debit_card_valid_from_year = "19";
+		public static string debit_card_expiry_month = "12";
+		public static string debit_card_expiry_year = "21";
+		public static string debit_card_cvc = "111";
+	}
+
+	public class EnrolledMasterCardCredit //checked
+	{
+		public static string credit_card_holder = "TestEnrolledMasterCardCredit";
+		public static string credit_card_number = "5411420000000002";
+		public static string credit_card_valid_from_month = "12";
+		public static string credit_card_valid_from_year = "19";
+		public static string credit_card_expiry_month = "12";
+		public static string credit_card_expiry_year = "21";
+		public static string credit_card_cvc = "111";
+	}
+
+	public class EnrolledAmex //checked
+	{
+		public static string amex_card_holder = "TestEnrolledAmex";
+		public static string amex_card_number = "373522010100107";
+		public static string amex_card_valid_from_month = "12";
+		public static string amex_card_valid_from_year = "19";
+		public static string amex_card_expiry_month = "12";
+		public static string amex_card_expiry_year = "21";
+		public static string amex_card_cvc = "1111";
+	}
+
+
 }
