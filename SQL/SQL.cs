c1d4103316da33c3ead3ac9407895dbbb5702019
maxmigov <maxim.migov@aspira.software>
2021-01-29 00:08:06 +0200

    new branch

--- SQL/SQL.cs
+++ SQL/SQL.cs
@@ -6,7 +6,7 @@ namespace CommifyMSTestFramework.SQL
 {
 	class Queries
 	{
-		public static String GetStatusByInstanceIdAndSessionId(int instanceID, string sessionID)
+		public static String TblPaymentsGetStatusByInstanceIdAndSessionId(int instanceID, string sessionID)
 		{
 			DataTable QueryResult = GetQueryResult(
 				"Data Source=192.168.110.2; Initial Catalog = IVR_BORDER_AGILE_TEST; User ID = ivr; Password = letmein",
@@ -22,6 +22,23 @@ namespace CommifyMSTestFramework.SQL
 			return Status;
 		}
 
+		public static String TblSoapsGetLookUpWebStatusByInstanceIdAndSessionId(int instanceID, string sessionID)
+		{
+			DataTable QueryResult = GetQueryResult(
+				"Data Source=192.168.110.2; Initial Catalog = IVR_BORDER_AGILE_TEST; User ID = ivr; Password = letmein",
+				 $"select * from tblPayments where InstanceId = {instanceID} and " +
+				 $"and event = 'LOOKUP-WEB' and reference = '{sessionID}' order by 1 desc");
+			// 4 is the index of Column of STATUS
+			//Bear in mind, if you want to pass a string into SQL query remember to add ' ' before and after the parameter
+			//e.g. and reference = '{sessionID}'
+			if (QueryResult.Rows.Count <= 0)
+			{
+				throw new ArgumentException("QueryResult is empty! No rows returned!");
+			}
+			String Status = QueryResult.Rows[0].ItemArray[7].ToString();
+			return Status;
+		}
+		/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		private static DataTable GetQueryResult(String vConnectionString, String vQuery)
 		{
 			SqlConnection Connection;  // It is for SQL connection
@@ -31,24 +48,25 @@ namespace CommifyMSTestFramework.SQL
 			{
 				Connection = new SqlConnection(vConnectionString);  // Declare SQL connection with connection string 
 				Connection.Open();  // Connect to Database
-				Console.WriteLine("✔✔✔Connected to DB✔✔✔");
+				Console.WriteLine("✔Connected to DB✔");
 				SqlDataAdapter adp = new SqlDataAdapter(vQuery, Connection);  // Execute query on database 
 				adp.Fill(ds);  // Store query result into DataSet object
-				Console.WriteLine("Data stored into DataSet");
+				Console.WriteLine("✔Data stored into DataSet✔");
 				Connection.Close();  // Close connection 
 				Connection.Dispose();   // Dispose connection 
-				Console.WriteLine("SQL connection disposed");
+				Console.WriteLine("✔SQL connection disposed✔");
 			}
 			catch (Exception E)
 			{
-				Console.WriteLine("✘✘✘Error in getting result of query✘✘✘");
+				Console.WriteLine("✘Error in getting result of query✘");
 				Console.WriteLine(E.Message);
 				return new DataTable();
 			}
 			return ds.Tables[0];
 		}
+		/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-		public static void DeleteFromTblSoapUsingSessionIdAndInstanceId(string sessionID, int instanceID)
+		public static void TblSoapDeleteUsingInstanceIdAndSessionId(int instanceID, string sessionID)
 		{
 			String conn = "Data Source=192.168.110.2; Initial Catalog = IVR_BORDER_AGILE_TEST; User ID = ivr; Password = letmein";
 			String query = $"delete from tblSOAP where Reference = '{sessionID}' and InstanceId = {instanceID}";
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -3,11 +3,10 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Threading;
-using Xunit;
+
 
 namespace CommifyMSTestFramework
 {
@@ -124,9 +123,27 @@ namespace CommifyMSTestFramework
 			webDriver = browser;
 			PageObjects.Elements homePage = new PageObjects.Elements(browser);
 			homePage.MaximiseWindow(browser);
-			SQL.Queries.DeleteFromTblSoapUsingSessionIdAndInstanceId("976156be-28fc-45ef-81dd-06fab93c6010", 1183);
-			Thread.Sleep(5000);
 			webDriver.Navigate().GoToUrl("https://npower.mysecurepay-int.co.uk/SessionId?=976156be-28fc-45ef-81dd-06fab93c6010");
+			if (webDriver.Title.Contains("Not Found"))
+			{
+				throw new ArgumentException("Cristina is doing a deploy on Agile Web :D");
+			}
+			if (webDriver.Url.Contains("completed"))
+			{
+				Console.WriteLine("SessionID has been already used. Deleting from tblSOAP...");
+				SQL.Queries.TblSoapDeleteUsingInstanceIdAndSessionId(1183 , "976156be-28fc-45ef-81dd-06fab93c6010");
+				webDriver.Navigate().GoToUrl("https://npower.mysecurepay-int.co.uk/SessionId?=976156be-28fc-45ef-81dd-06fab93c6010");
+			}
+			String lookUpWeb = SQL.Queries.TblSoapsGetLookUpWebStatusByInstanceIdAndSessionId(1183, "976156be-28fc-45ef-81dd-06fab93c6010");
+			if (lookUpWeb.Equals("FAILED"))
+			{
+				throw new ArgumentException("LOOKUP-WEB status is FAILED!");
+			}
+			else
+			{
+				Console.WriteLine("Status for LOOKUP-WEB: " + lookUpWeb);
+			}
+			//Thread.Sleep(5000);
 			homePage.EnterCardHolderName(TestCards.PaySafe3DS.EnrolledVisaCredit.credit_card_holder);
 			homePage.EnterCardNumber(TestCards.PaySafe3DS.EnrolledVisaDebit.debit_card_number);
 			homePage.EnterExpiryMonth(TestCards.PaySafe3DS.EnrolledVisaDebit.debit_card_expiry_month);
@@ -158,8 +175,8 @@ namespace CommifyMSTestFramework
 			TestCards.PaySafe3DS.ChallengePage challenge = new TestCards.PaySafe3DS.ChallengePage(webDriver);
 			challenge.AuthenticationSuccessful();
 			Assert.IsTrue(webDriver.Title.Contains("completed"));
-			var statustblPayment = SQL.Queries.GetStatusByInstanceIdAndSessionId(1183, "976156be-28fc-45ef-81dd-06fab93c6010");
-			Console.WriteLine(statustblPayment);
+			var statustblPayment = SQL.Queries.TblPaymentsGetStatusByInstanceIdAndSessionId(1183, "976156be-28fc-45ef-81dd-06fab93c6010");
+			Console.WriteLine("Status from tblPayments: " + statustblPayment);
 			Assert.AreEqual("SUCCESS", statustblPayment);
 		}
 
